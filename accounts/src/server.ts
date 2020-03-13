75527c2284cc0854fddcb54f085e25c83bc4e63f
Paulo Eduardo <pauloes.dev@gmail.com>
2020-03-12 23:35:15 -0300

    removing node_modules

--- accounts/src/server.ts
+++ accounts/src/server.ts
@@ -1,6 +1,8 @@
 import App from "./app";
 import HealthcheckController from "./healthcheck/healthcheck.controller";
+import validateEnv from "./utils/validateEnv";
 
+validateEnv();
 const app = new App([new HealthcheckController()], 5000);
 
 app.listen();
